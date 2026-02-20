ab076a208ca06d4a3d2e6f2e389a2460f9904da5
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 15:21:15 +0530

    Initialize Serial communication in setup function

--- src/main.cpp
+++ src/main.cpp
@@ -17,3 +17,15 @@ const int LDR_PIN = A0;
 // TODO 2:
 // Create variable to store sensor reading
 int ldrValue = 0;
+
+void setup() {
+
+    // TODO 3:
+    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
+
+    // TODO 4:
+    // Print system initialization message
+    Serial.println("LDR Light Intensity Monitoring System Initialized");
+    Serial.println("Monitoring ambient light levels...");
+}
